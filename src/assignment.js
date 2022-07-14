2d0b8672301510c473cc7540b1eea5b366c2baea
FuzzyLojik <101531511+chibunduandy@users.noreply.github.com>
2022-07-14 23:58:06 +0000

    check assignments 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,12 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+    for (let i = 0; i < arrayOfNumbers.length; i++)
+    sum += arrayOfNumbers[i]
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +31,14 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let counter = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++)
+    if (arrayOfNumbers[i] % 2 === 0) {
+      counter +=1
+    }
+    return counter;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
